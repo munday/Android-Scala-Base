746b5b0e016ecfd5d5deefa11ef668a943a18ab6
Matt Munday <munday@gmail.com>
2012-08-29 10:06:10 -0400

    updates for android sdk tools 20.0.1 on mac

--- gen/R.java.d
+++ gen/R.java.d
@@ -4,4 +4,7 @@
 /Users/matt/Documents/workspace/Android-Scala-Base/res/drawable-mdpi/icon.png \
 /Users/matt/Documents/workspace/Android-Scala-Base/res/layout/main.xml \
 /Users/matt/Documents/workspace/Android-Scala-Base/res/values/strings.xml \
-/Users/matt/Documents/workspace/Android-Scala-Base/AndroidManifest.xml \
+/Users/matt/Documents/workspace/Android-Scala-Base/bin/res/drawable-hdpi/icon.png \
+/Users/matt/Documents/workspace/Android-Scala-Base/bin/res/drawable-ldpi/icon.png \
+/Users/matt/Documents/workspace/Android-Scala-Base/bin/res/drawable-mdpi/icon.png \
+/Users/matt/Documents/workspace/Android-Scala-Base/bin/AndroidManifest.xml \
