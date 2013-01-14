a6a3d26312f7d37a3771d8b2919463c27e1d2299
Matt Munday <munday@gmail.com>
2013-01-13 21:26:50 -0500

    fix mac build issues, rebased the support configs to use the ant-android-scala dir instead of having to copy it elsewhere

--- gen/R.java.d
+++ gen/R.java.d
@@ -4,7 +4,4 @@
 /Users/matt/Documents/workspaces/android-scala-base/Android-Scala-Base/res/drawable-mdpi/icon.png \
 /Users/matt/Documents/workspaces/android-scala-base/Android-Scala-Base/res/layout/main.xml \
 /Users/matt/Documents/workspaces/android-scala-base/Android-Scala-Base/res/values/strings.xml \
-/Users/matt/Documents/workspaces/android-scala-base/Android-Scala-Base/bin/res/drawable-hdpi/icon.png \
-/Users/matt/Documents/workspaces/android-scala-base/Android-Scala-Base/bin/res/drawable-ldpi/icon.png \
-/Users/matt/Documents/workspaces/android-scala-base/Android-Scala-Base/bin/res/drawable-mdpi/icon.png \
 /Users/matt/Documents/workspaces/android-scala-base/Android-Scala-Base/bin/AndroidManifest.xml \
