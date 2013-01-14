3dd5d044e9ab18e2acf676dd1ebb7be1d4cb97e8
Matt Munday <munday@gmail.com>
2013-01-13 21:16:45 -0500

    make scala 2.9.2 work again

--- gen/R.java.d
+++ gen/R.java.d
@@ -1,10 +1,7 @@
-/Users/matt/Documents/workspace/Android-Scala-Base/gen/ws/munday/androidscalabase/R.java \
- : /Users/matt/Documents/workspace/Android-Scala-Base/res/drawable-hdpi/icon.png \
-/Users/matt/Documents/workspace/Android-Scala-Base/res/drawable-ldpi/icon.png \
-/Users/matt/Documents/workspace/Android-Scala-Base/res/drawable-mdpi/icon.png \
-/Users/matt/Documents/workspace/Android-Scala-Base/res/layout/main.xml \
-/Users/matt/Documents/workspace/Android-Scala-Base/res/values/strings.xml \
-/Users/matt/Documents/workspace/Android-Scala-Base/bin/res/drawable-hdpi/icon.png \
-/Users/matt/Documents/workspace/Android-Scala-Base/bin/res/drawable-ldpi/icon.png \
-/Users/matt/Documents/workspace/Android-Scala-Base/bin/res/drawable-mdpi/icon.png \
-/Users/matt/Documents/workspace/Android-Scala-Base/bin/AndroidManifest.xml \
+/Users/matt/Documents/workspaces/android-scala-base/Android-Scala-Base/gen/ws/munday/androidscalabase/R.java \
+ : /Users/matt/Documents/workspaces/android-scala-base/Android-Scala-Base/res/drawable-hdpi/icon.png \
+/Users/matt/Documents/workspaces/android-scala-base/Android-Scala-Base/res/drawable-ldpi/icon.png \
+/Users/matt/Documents/workspaces/android-scala-base/Android-Scala-Base/res/drawable-mdpi/icon.png \
+/Users/matt/Documents/workspaces/android-scala-base/Android-Scala-Base/res/layout/main.xml \
+/Users/matt/Documents/workspaces/android-scala-base/Android-Scala-Base/res/values/strings.xml \
+/Users/matt/Documents/workspaces/android-scala-base/Android-Scala-Base/bin/AndroidManifest.xml \
